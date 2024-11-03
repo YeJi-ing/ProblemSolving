758aa7c9bf8feb43bb46c857219e93cc87bc2830
YeJi-ing <ping_yeji@naver.com>
2024-11-03 20:57:51 +0900

    solve: 1978

---
+++ Part1/1978.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+
+N = int(input())
+arr = [*map(int, input().split())]
+ans = 0
+
+for i in range(N):
+    non = 0
+    for j in range(2, arr[i]):
+        if arr[i] % j == 0:
+            non += 1
+            break
+    if non == 0: # 소수
+        ans += 1
+
+print(ans)
